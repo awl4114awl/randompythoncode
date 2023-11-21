dcdd273c9ea3fef6a900001172999ff1905507f7
jordan calvert <149392624+awl4114awl@users.noreply.github.com>
2023-11-21 06:16:43 -0800

    LargerSum.py

--- LargerSum.py
+++ LargerSum.py
@@ -30,3 +30,8 @@ def larger_sum(lst1, lst2):
 
 #Uncomment the line below when your function is done
 print(larger_sum([1, 9, 5], [2, 3, 7]))
+
+In this solution, we manually iterate through each element in each list and calculate our sums. We then 
+return the list with the greater sum and break the tie by returning lst1. You can also try solving this 
+problem using the sum() function in python. The body of this function could also be condensed into one 
+line of code if you want an additional challenge!
